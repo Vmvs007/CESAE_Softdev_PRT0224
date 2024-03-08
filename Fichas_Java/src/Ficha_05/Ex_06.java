9ca988d20f9a4570c446e8c39e721c1b0947f7c6
Vitor Santos <vmvs007@gmail.com>
2024-03-08 14:25:45 +0000

    Ficha_05

--- Fichas_Java/src/Ficha_05/Ex_06.java
+++ Fichas_Java/src/Ficha_05/Ex_06.java
@@ -1,4 +1,41 @@
 package Ficha_05;
 
+import java.util.Scanner;
+
 public class Ex_06 {
+    public static void main(String[] args) {
+        // Importar o Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int[] vetor = new int[10];
+        boolean crescente = true;
+
+        System.out.println("**************************");
+
+        // Ler vetor
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Insira um número no vetor[" + i + "]: ");
+            vetor[i] = input.nextInt();
+        }
+
+        System.out.println("**************************");
+
+
+        for (int i = 1; i < vetor.length; i++) {
+
+            if (vetor[i] <= vetor[i - 1]) {
+                // O vetor deixou de ser crescente
+                crescente = false;
+            }
+        }
+
+        if (crescente) {
+            System.out.println("O vetor é crescente");
+        } else {
+            System.out.println("O vetor não é crescente");
+        }
+
+
+    }
 }
